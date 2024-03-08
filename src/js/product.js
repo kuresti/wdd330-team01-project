d05f6a6f97e73afe005d7d21c2221e574b8ce29e
CabrynBray <cabrynbrayza@gmail.comgit config --global user.name>
2024-03-08 17:26:21 +0200

    Cabryn's completed remove from cart feature. (everything works)

--- src/js/product.js
+++ src/js/product.js
@@ -11,21 +11,3 @@ const productId = getParams("product");
 const product = new ProductDetails(productId, dataSource);
 product.init();
 
-
-// add to cart button event handler
-/*async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
-
-
-
-
-//test findProductById function
-console.log(dataSource.findProductById(productId));*/
-
