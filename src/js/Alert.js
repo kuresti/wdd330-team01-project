49eb3aed12fb66201766de2ef8c100c4ad8bf132
LisaMandarin <chu23001@byui.edu>
2024-03-12 02:23:28 +0800

    alert message works on top of main.

--- src/js/Alert.js
+++ src/js/Alert.js
@@ -1,20 +1,36 @@
 function convertToJson(res) {
-    if (res.ok) {
-        return res.json();
-    } else {
-        throw new Error("Bad Response");
-    }
+  if (res.ok) {
+    return res.json();
+  } else {
+    throw new Error("Bad Response");
+  }
 }
-export default class Alert {
-    async getAlertData(){
-        const response = fetch("../json/alert.json");
-        const data = await convertToJson(response);
-        return data;
-    }
-    displayAlert() {
-        const alertElement = document.createElement("section");
-        alertElement.classList.add("alert-list");
-        const alerts = this.getAlertData().map((alert) => )
-    }
 
+export default class Alert {
+  async getAlertData() {
+    const response = await fetch("../json/alert.json");
+    const data = await convertToJson(response);
+    return data;
+  }
+
+  renderMessage(msg) {
+    const main = document.querySelector("main")
+    const section = document.createElement("section");
+    section.classList.add("alert-list");
+    const p = document.createElement("p");
+    p.textContent = msg.message;
+    p.style.backgroundColor = msg.background;
+    p.style.color = msg.color;
+    section.appendChild(p);
+    main.prepend(section);
+  }
+
+  async displayAlert() {
+    const alertElement = document.createElement("section");
+    alertElement.classList.add("alert-list");
+    const messages = await this.getAlertData();
+    messages.map((msg)=> this.renderMessage(msg));
+    }
 }
+const alert = new Alert();
+alert.displayAlert();
