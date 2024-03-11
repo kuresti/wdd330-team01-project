b4e0e70ad906788fbd91a3fea81e33139b0d1fcb
CabrynBray <cabrynbrayza@gmail.comgit config --global user.name>
2024-03-11 17:14:59 +0200

    CB completed W03 team activity (Ran npm Lint and Format)

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,4 +1,8 @@
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+import {
+  getLocalStorage,
+  setLocalStorage,
+  loadHeaderFooter,
+} from "./utils.mjs";
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
@@ -69,3 +73,4 @@ function removeItemFromCart(index) {
 
 renderCartContents();
 checkCartItems();
+loadHeaderFooter();
