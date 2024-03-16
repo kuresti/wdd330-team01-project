57f86dd85ed8038326947932949f35f218e7896a
kuresti <ure17002@byui.edu>
2024-03-16 15:47:26 -0600

    Increment quantity form

--- src/js/cart.js
+++ src/js/cart.js
@@ -25,7 +25,13 @@ function cartItemTemplate(item, index) {
     <h2 class="card__name">${item.Name}</h2>
   </a>
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: 1</p>
+  <form class"car-card__quantity-controls">
+  <label for="quantity" class="quantity">Quantity</label>
+  <div class="value-button" id="quantity-increment" value="Increase Value">+</div>
+  <input type="number" class="quantity" name="quantity" value="1" min="1" readonly>
+  <div class="value-button" id="quantity-decrement" value="Decrease Value">-</div>
+  </form>
+
   <p class="cart-card__price">$${item.FinalPrice}</p>
   <button class="remove-item" data-index="${index}">X</button>
 </li>`;
