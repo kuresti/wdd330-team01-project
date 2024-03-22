5a6d166e36920809d493c6768e015b3cefc6d3fb
LisaMandarin <chu23001@byui.edu>
2024-03-23 00:21:04 +0800

    format

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -6,22 +6,21 @@ loadHeaderFooter();
 function setExpirationPeriod() {
   let today = new Date();
   let year = today.getFullYear();
-  let futureYear = year + 10
+  let futureYear = year + 10;
   let month = today.getMonth() + 1;
   month = month < 10 ? "0" + month : month.toString();
   let currentMonth = year + "-" + month;
   let futureMonth = futureYear + "-" + month;
-  
+
   document.getElementById("expiration").setAttribute("min", currentMonth);
   document.getElementById("expiration").setAttribute("max", futureMonth);
 }
 
 setExpirationPeriod();
 
-
 function formValidation() {
   document.addEventListener("DOMContentLoaded", () => {
-    const form = document.forms["checkout"]
+    const form = document.forms["checkout"];
     form.addEventListener("change", (event) => {
       const target = event.target;
       const errorMsg = event.srcElement.nextElementSibling;
@@ -30,10 +29,10 @@ function formValidation() {
       } else {
         errorMsg.classList.add("hide");
       }
-      });
+    });
   });
 }
-formValidation()
+formValidation();
 
 const myCheckout = new CheckoutProcess("so-cart", ".checkout-summary");
 
@@ -45,7 +44,7 @@ document
 
 document.querySelector("#checkoutSubmit").addEventListener("click", (e) => {
   e.preventDefault();
-  
+
   const formElem = document.forms["checkout"];
   const isValid = formElem.checkValidity();
   if (!isValid) {
