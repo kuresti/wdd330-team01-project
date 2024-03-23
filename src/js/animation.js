b9957c989f98c3fdc9c2f149dc0c7a2ee5fa2f4d
kuresti <ure17002@byui.edu>
2024-03-23 05:37:28 -0600

    animation

---
+++ src/js/animation.js
@@ -0,0 +1,12 @@
+const cartIcon = document.querySelector(".icon-animation");
+const addToCartButtons = document.querySelectorAll("#addToCart");
+
+addToCartButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        cartIcon.classList.add("animate");
+
+        setTimeout(() => {
+            cartIcon.classList.remove("animate");
+        },  500);
+    });
+});
